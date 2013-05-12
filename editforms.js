9d16d57a90c2629f7e7911b9d6f4afe9bff26eeb
Cameron Stalder <stalder1@umbc.edu>
2013-05-11 21:38:44 -0400

    editform update

--- editforms.js
+++ editforms.js
@@ -1,22 +1,22 @@
 function populateFields(refID){
 
-var F_NAME = getElementById('F_NAME_' + refID)
-var L_NAME =  getElementById('L_NAME_' + refID)
-var TITLE =  getElementById('TITLE_' + refID)
-var COMPANY =  getElementById('COMPANY_' + refID)
-var ADDRESS =  getElementById('ADDRESS_' + refID)
-var EMAIL =  getElementById('EMAIL_' + refID)
-var PHONE_NUMBER =  getElementById('PHONE_NUMBER_' + refID)
-var DISPLAY_RANK_ID =  getElementById('DISPLAY_RANK_ID_' + refID)
+var F_NAME = getElementById('F_NAME_' + refID);
+var L_NAME =  getElementById('L_NAME_' + refID);
+var TITLE =  getElementById('TITLE_' + refID);
+var COMPANY =  getElementById('COMPANY_' + refID);
+var ADDRESS =  getElementById('ADDRESS_' + refID);
+var EMAIL =  getElementById('EMAIL_' + refID);
+var PHONE_NUMBER =  getElementById('PHONE_NUMBER_' + refID);
+var DISPLAY_RANK_ID =  getElementById('DISPLAY_RANK_ID_' + refID);
 
-document.getElementById('F_NAME_IN').setAttribute("value", F_NAME)
-document.getElementById('L_NAME_IN').setAttribute("value", L_NAME)
-document.getElementById('PHONE_NUMBER_IN').setAttribute("value", TITLE)
-document.getElementById('ADDRESS_IN').setAttribute("value", COMPANY)
-document.getElementById('EMAIL_IN').setAttribute("value", ADDRESS)
-document.getElementById('COMPANY_IN').setAttribute("value", EMAIL)
-document.getElementById('TITLE_IN').setAttribute("value", PHONE_NUMBER)
-document.getElementById('DISPLAY_IN').setAttribute("value", DISPLAY_RANK_ID)
+document.getElementById('F_NAME_IN').setAttribute("value", F_NAME);
+document.getElementById('L_NAME_IN').setAttribute("value", L_NAME);
+document.getElementById('PHONE_NUMBER_IN').setAttribute("value", TITLE);
+document.getElementById('ADDRESS_IN').setAttribute("value", COMPANY);
+document.getElementById('EMAIL_IN').setAttribute("value", ADDRESS);
+document.getElementById('COMPANY_IN').setAttribute("value", EMAIL);
+document.getElementById('TITLE_IN').setAttribute("value", PHONE_NUMBER);
+document.getElementById('DISPLAY_IN').setAttribute("value", DISPLAY_RANK_ID);
 }
 
 
